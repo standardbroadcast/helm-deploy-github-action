888d7654e4e05b9df3d8202a80a526e76798dc44
Luciano Issoe <luciano@nebula.tv>
2024-01-24 16:20:19 -0300

    Adds process.env to helm exec

--- index.js
+++ index.js
@@ -266,7 +266,7 @@ async function run() {
           AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID ,
           AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY ,
           AWS_DEFAULT_REGION: "us-east-1",
-          KUBECONFIG: "./kubeconfig.yml"
+          ...process.env
         },
       });
     }
