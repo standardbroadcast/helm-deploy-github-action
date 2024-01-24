51a78d7a8c719fc0f59239baedeb6a39b775f9e4
Luciano Issoe <luciano@nebula.tv>
2024-01-24 14:41:59 -0300

    Passes env to exec.exec

--- index.js
+++ index.js
@@ -261,9 +261,7 @@ async function run() {
     } else {
       await exec.exec(helm, args, {
         env: {
-          AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-          AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-          AWS_DEFAULT_REGION: process.env.AWS_DEFAULT_REGION
+          ...process.env
         },
       });
     }
